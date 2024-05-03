6faab14864f74236baaa323160bb8a481985a4fd
Satyam Singh <trueleo@protonmail.com>
2024-05-03 22:13:37 +0530

    Add  basic keymaps and settings

--- init.lua
+++ init.lua
@@ -190,6 +190,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- My Config
+vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-s>', ':update<Enter>:Format<Enter>', { silent = true })
+vim.o.number = true;
+vim.o.relativenumber = true;
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
